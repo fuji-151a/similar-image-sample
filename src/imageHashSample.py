645ff5c0ea4f91a72518aad6db328dc4b53f1243
fuji-151a <life.of.151a@gmailcom>
2023-02-15 09:38:40 +0900

    pathの修正

--- src/imageHashSample.py
+++ src/imageHashSample.py
@@ -1,9 +1,9 @@
 from PIL import Image
 import imagehash
 
-hash = imagehash.average_hash(Image.open('/mnt/c/Users/fuji_/git/similar-image/image/ms4_1.png'))
+hash = imagehash.average_hash(Image.open('/mnt/c/Users/fuji_/git/similar-image-sample/image/ms4_1.png'))
 print(hash)
-otherhash = imagehash.average_hash(Image.open('/mnt/c/Users/fuji_/git/similar-image/image/ms4_4.png'))
+otherhash = imagehash.average_hash(Image.open('/mnt/c/Users/fuji_/git/similar-image-sample/image/ms4_5.jpg'))
 print(otherhash)
 
 print(hash == otherhash)
